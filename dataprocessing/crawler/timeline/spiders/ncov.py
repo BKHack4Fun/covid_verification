4b0d4abdf4f5803361721c119220e87c2f107e02
NamDH <dinhhoangnam998@gmail.com>
2020-06-26 14:54:16 +0700

    crawl increment done!

--- dataprocessing/crawler/timeline/spiders/ncov.py
+++ dataprocessing/crawler/timeline/spiders/ncov.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-# from datetime import datetime
 
 class NcovSpider(scrapy.Spider):
     name = 'ncov'
@@ -11,11 +10,10 @@ class NcovSpider(scrapy.Spider):
         timeline = response.xpath("/html/body/div[1]/div/div/div[2]/div/div/div/div/div/div/div[1]/div/div/section/div/div/div/div[3]/div/div/div/div/ul")
         for news in timeline:
             time = news.css("div.timeline-detail > div.timeline-head > h3::text").get()
-            # time = datetime.strptime(time, r"%H:%M %d/%m/%Y")
             content = news.css("div.timeline-detail > div.timeline-content > p::text").get()
             link = response.request.url.split('?')[0]
             yield {'time': time, 'content': content, 'link': link}
 
-        next_page = response.xpath("/html/body/div[1]/div/div/div[2]/div/div/div/div/div/div/div[1]/div/div/section/div/div/div/div[4]/ul/li[2]/a")
-        yield from response.follow_all(next_page)
+        # next_page = response.xpath("/html/body/div[1]/div/div/div[2]/div/div/div/div/div/div/div[1]/div/div/section/div/div/div/div[4]/ul/li[2]/a")
+        # yield from response.follow_all(next_page)
 
